ead48cef6ffa0e023fcd5c0a0c6b4715c99e6c59
Victor Fernandez <vfernandezgabriel@zscaler.com>
2025-11-03 18:38:52 +0100

    refactor: move things around

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { parameters } from "./libs/args";
 import { createHardLink } from "./libs/createHardLink";
 import { createFileWatcher } from "./libs/fileWatcher";
 import { isVideoFile } from "./libs/filterVideoFiles";
+import { fuzzyMatch } from "./libs/fuzzyMatcher";
 import { logger } from "./libs/logger";
 import {
   parseVideoFileName,
@@ -19,74 +20,7 @@ watcher.on("ready", () => {
 async function handleFileCreated({ filePath }: { filePath: string }) {
   if (!isVideoFile(filePath)) return;
 
-  let info: EpisodeInfo | null = null;
-  try {
-    info = parseVideoFileName(filePath);
-  } catch (error) {
-    logger.error(error);
-    return;
-  }
-
-  const searchResult = await searchShow(
-    info!.showName,
-    parameters["tmdb-token"]
-  );
-
-  let foundMatch = false;
-  for (const show of searchResult) {
-    if (show.names.includes(info!.showName.toLowerCase())) {
-      info.showName = show.name;
-      foundMatch = true;
-    }
-  }
-
-  if (foundMatch) {
-    logger.debug(`Exact match found for: "${info!.showName}"`);
-  }
-
-  if (!foundMatch) {
-    logger.debug(
-      `Not found exact match for "${
-        info!.showName
-      }". Trying without diacritics.`
-    );
-    const parsedNameWithoutDiacritics = removeDiacritics(
-      info!.showName.toLowerCase()
-    );
-    for (const show of searchResult) {
-      const apiNamesWithoutDiacritics = show.names.map((e) =>
-        removeDiacritics(e)
-      );
-      if (apiNamesWithoutDiacritics.includes(parsedNameWithoutDiacritics)) {
-        info.showName = show.name;
-        foundMatch = true;
-        logger.debug(
-          `Found a match after removing diacritics: "${parsedNameWithoutDiacritics}" in "${JSON.stringify(
-            apiNamesWithoutDiacritics
-          )}"`
-        );
-      }
-    }
-
-    if (foundMatch) {
-      logger.debug(
-        `Exact match (after removing diacritics) found for: ${info!.showName}`
-      );
-    }
-  }
-
-  if (!foundMatch) {
-    if (searchResult.length === 0) {
-      logger.warn("Could not found any match for name:", info!.showName);
-    } else {
-      logger.warn(
-        `Could not find exact match for: "${
-          info!.showName
-        }" . Using first search result: "${searchResult[0]!.name}`
-      );
-      info.showName = searchResult[0]!.name;
-    }
-  }
+  const info: EpisodeInfo = await fuzzyMatch(filePath);
 
   await createHardLink(filePath, parameters["sorted-directory"], info!);
 }
